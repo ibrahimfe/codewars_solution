435fc9e248a9938c9e3663d220dfe63c7589d444
ibrahimfe <ibrahiimm.f@gmail.com>
2023-04-26 19:07:44 +0700

    learn about list comprehension and enumerate

---
+++ eureka.py
@@ -0,0 +1,16 @@
+# The One-Liner as Always
+def sum_dig_pow(a, b):
+    return [
+        i
+        for i in range(a, b + 1)
+        if i == sum(int(digit) ** (index + 1) for index, digit in enumerate(str(i)))
+    ]
+
+
+def sum_dig_pow(a, b):  # range(a, b + 1) will be studied by the function
+    result = []
+    for i in range(a, b + 1):
+        eureka = sum(int(e) ** index for index, e in (enumerate(str(i), start=1)))
+        if eureka == i:
+            result.append(i)
+    return result

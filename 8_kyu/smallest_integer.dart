a71c9e4745921a8d4ce4092993d322b3311c2e21
ibrahimfe <ibrahiimm.f@gmail.com>
2023-09-09 10:30:25 +0700

    smallest integer using reduce() and min

--- 8_kyu/smallest_integer.dart
+++ 8_kyu/smallest_integer.dart
@@ -1,9 +1,5 @@
+import 'dart:math';
+
 int findSmallestInt(List<int> arr) {
-  int smallest_number = arr[0];
-  for (int i = 1; i < arr.length; i++) {
-    if (arr[i] < smallest_number) {
-      smallest_number = arr[i];
-    }
-  }
-  return smallest_number;
+  return arr.reduce(min);
 }
